388b41ec1e183198d71461612f87ce5bf8d64aa9
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-09-05 04:58:12 -0300

    Update

--- src/components/postagens/listapostagens/ListaPostagens.tsx
+++ src/components/postagens/listapostagens/ListaPostagens.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import CardPostagens from "../cardPostagens/CardPostagens";
+import CardPostagens from "../cardpostagens/CardPostagens";
 import { useState, useContext, useEffect } from "react";
 import { AuthContext } from "../../../contexts/AuthContext";
 import Postagem from "../../../models/Postagem";
